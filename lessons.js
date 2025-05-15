9a3e511e2c4c1bcd7780338c3d272a740317291c
rutauiska <ruuta.uiska@gmail.com>
2025-05-15 20:36:47 +0300

    Create lessons.js

---
+++ lessons.js
@@ -0,0 +1,5 @@
+const lessons = [
+  { key: "js", title: "JavaScript Basics", file: "lesson_tasks_js.js" },
+  { key: "html", title: "HTML Fundamentals", file: "lesson_tasks_html.js" },
+  { key: "css", title: "CSS Styling", file: "lesson_tasks_css.js" }
+];
